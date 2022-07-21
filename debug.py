328d3f39150fa40148ddf0ddee0177aa40d0c917
chLi <565353780@qq.com>
2022-07-21 10:51:29 +0800

    add debug code

---
+++ debug.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from Dataset.udf_dataset import UDFDataset
+
+udf_root_folder_path = "/home/chli/scan2cad/im3d_udf/"
+
+dataset = UDFDataset(udf_root_folder_path)
+
